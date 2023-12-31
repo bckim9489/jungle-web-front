c2f8adaedff9dd2d4345764745ecaf65e2747501
bckim9489 <bckim9489@gmail.com>
2023-12-31 22:18:09 +0900

    주석제거

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/v1", {
-      target: `${process.env.REACT_APP_API_URL}`, //로컬
+      target: `${process.env.REACT_APP_API_URL}`,
       changeOrigin: true,
     })
   );
