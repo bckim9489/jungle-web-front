7d4c0073f51fc09c663c0bc1ac80762dd05302e0
bckim9489 <bckim9489@gmail.com>
2024-01-10 07:19:47 +0900

    여백수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,7 @@ import '../App.css';
 const Home = () => {
   const imgUrl = "https://github.com/bckim9489/JungleWeb/assets/47053587/d1256cf5-19ef-4848-a36f-51eb37b3c3f0"
   return (
-    <div style={{textAlign: "center", minHeight: "60vh", margin: "10vh"}}>
+    <div style={{textAlign: "center", minHeight: "30vh", margin: "10vh"}}>
       <h1>JungleWeb</h1>
       <br/>
       <br/>
