0f3c73a08a1cfd218f2557150ea8986cb3c9a667
bckim9489 <bckim9489@gmail.com>
2024-01-10 06:54:37 +0900

    메인배경

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 const Home = () => {
+  const imgUrl = "https://github.com/bckim9489/JungleWeb/assets/47053587/d1256cf5-19ef-4848-a36f-51eb37b3c3f0"
   return (
     <div>
-      홈 화면 입니다.
+      <img src={imgUrl}/>
     </div>
   );
 };
